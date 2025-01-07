7276a7b8953ffc189fcee6e8ed0b2cb5aee15965
Xenia <xeniavislocka@gmail.com>
2025-01-07 18:14:03 +0100

    Initial commit - structure

---
+++ include/philosophers.h
@@ -0,0 +1,14 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+/* *** libraries *** */
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdarg.h>
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "../include/philosophers.h"
+
+int main(void)
+{
+    printf("Hello world");
+    return (0);
+}
